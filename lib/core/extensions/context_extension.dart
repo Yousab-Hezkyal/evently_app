103df6dbf9db46d7492a4320ed696e2866b44dc7
Yousab Hezkyal <yousabhezkayl@gmail.com>
2025-10-02 09:03:47 +0300

    Initial Flutter project

---
+++ lib/core/extensions/context_extension.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  T? arg<T>() => ModalRoute.of(this)?.settings.arguments as T?;
+
+  double get height => MediaQuery.sizeOf(this).height;
+  double get width => MediaQuery.sizeOf(this).width;
+
+  double dynamicHeight(double val) => height * val;
+  double dynamicWidth(double val) => width * val;
+}
